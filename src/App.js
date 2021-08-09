45076d13c4f0461767c5232a57200a3ec01df6f7
Dave Inoc <michael828inoc@gmail.com>
2021-08-09 22:52:45 +0800

    red

--- src/App.js
+++ src/App.js
@@ -1,154 +1,32 @@
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  Pressable,
-  Alert,
-  ToastAndroid,
-  Modal,
-  Image,
-  ImageBackground,
-} from 'react-native';
-import CustomButton from './components/CustomButton';
-import Header from './components/Header';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {View} from 'react-native';
+
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  const [name, SetName] = useState('');
-  const [submitted, SetSubmitted] = useState(false);
-  const [showWarning, setShowWarning] = useState(false);
-
-  const onPressHandler = () => {
-    if (name.length > 3) {
-      SetSubmitted(!submitted);
-    } else {
-      setShowWarning(true);
-    }
-  };
-
   return (
-    <ImageBackground
-      style={styles.body}
-      source={{
-        uri: 'https://cdn.pixabay.com/photo/2013/07/12/12/35/texture-145968_960_720.png',
-      }}>
-      <Header />
-      <Modal
-        visible={showWarning}
-        transparent
-        onRequestClose={() => SetshowWarning(false)}
-        animationType="slide"
-        hardwareAccelerated>
-        <View style={styles.centered_view}>
-          <View style={styles.warning_modal}>
-            <View style={styles.warning_title}>
-              <Text style={styles.text}>WARNING!</Text>
-            </View>
-            <View style={styles.warning_body}>
-              <Text style={styles.text}>
-                The name must be longer than 3 charachters
-              </Text>
-            </View>
-            <Pressable
-              onPress={() => setShowWarning(false)}
-              style={styles.warning_button}
-              android_ripple={{color: '#fff'}}>
-              <Text style={styles.text}>OK</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Text style={styles.text}>Please write your name:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="e.g. John"
-        onChangeText={value => SetName(value)}
-      />
-      <CustomButton
-        onPressFunction={onPressHandler}
-        title={submitted ? 'Clear' : 'Submit'}
-      />
-      {submitted ? (
-        <View style={styles.body}>
-          <Text style={styles.text}>You are registered as {name}</Text>
-          <Image
-            resizeMode="stretch"
-            style={styles.image}
-            source={require('./assets/done.png')}
-          />
-        </View>
-      ) : (
-        <Image
-          resizeMode="stretch"
-          style={styles.image}
-          source={require('./assets/error.png')}
-        />
-      )}
-    </ImageBackground>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen_A" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   text: {
-    color: '#000000',
-    fontSize: 20,
-    margin: 10,
-    textAlign: 'center',
-  },
-  input: {
-    width: 200,
-    borderWidth: 1,
-    borderColor: '#555',
-    borderRadius: 5,
-    textAlign: 'center',
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  button: {
-    width: 150,
-    height: 50,
-    alignItems: 'center',
-  },
-  centered_view: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00000099',
-  },
-  warning_modal: {
-    width: 300,
-    height: 300,
-    backgroundColor: '#ffffff',
-    borderWidth: 1,
-    borderColor: '#000',
-    borderRadius: 20,
-  },
-  warning_title: {
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ff0',
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
-  },
-  warning_body: {
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  warning_button: {
-    backgroundColor: '#00ffff',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-  },
-  image: {
-    width: 100,
-    height: 100,
+    fontSize: 40,
+    fontWeight: 'bold',
     margin: 10,
   },
 });
