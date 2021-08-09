e56aac3d3964d495859639a274e84ee3eaee3c51
Dave Inoc <michael828inoc@gmail.com>
2021-08-09 20:24:16 +0800

    refresh control

--- App.js
+++ App.js
@@ -1,40 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 
-import {Button, Linking, StyleSheet, Text, View} from 'react-native';
+import {
+  Button,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 const App = () => {
+  const [items, setItems] = React.useState([
+    {key: 1, item: 'item 1'},
+    {key: 2, item: 'item 2'},
+    {key: 3, item: 'item 3'},
+    {key: 4, item: 'item 4'},
+    {key: 5, item: 'item 5'},
+    {key: 6, item: 'item 5'},
+    {key: 7, item: 'item 5'},
+    {key: 8, item: 'item 5'},
+    {key: 9, item: 'item 5'},
+    {key: 10, item: 'item 5'},
+    {key: 11, item: 'item 5'},
+    {key: 12, item: 'item 5'},
+  ]);
+
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...items, {key: 69, item: 'POTANGINA MO'}]);
+    setRefreshing(false);
+  };
+
   return (
-    <View style={styles.body}>
-      <Text style={styles.text}>Testing</Text>
-      <Button
-        title="Tap me!"
-        onPress={() => {
-          Linking.openURL('https://youtube.com');
-        }}
-      />
-    </View>
+    <ScrollView
+      horizontal={false}
+      style={styles.body}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+      {items.map(item => (
+        <View style={styles.item}>
+          <Text style={styles.text} key={item.key}>
+            {item.item}
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
+    backgroundColor: '#fff',
+  },
+  item: {
+    margin: 10,
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#333',
   },
   text: {
     color: '#fff',
     fontWeight: 'bold',
-    fontSize: 20,
+    fontSize: 35,
     margin: 10,
   },
 });
